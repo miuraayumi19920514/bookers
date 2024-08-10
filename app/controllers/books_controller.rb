b79ecf8d176985dd8b30fbb269d397154161c0fb
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-10 19:16:46 +0000

    [機能追加]保存機能を追加。コントローラーに保存機能のcriateを記述しparamsも定義した。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,12 @@ class BooksController < ApplicationController
   def new
     @book=Book.new
   end
+  
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
+  end
 
   def index
   end
@@ -11,4 +17,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
