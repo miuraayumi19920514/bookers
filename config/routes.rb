5c25b700000351d1b6d0cab8efa12e28ec908680
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-10 08:30:48 +0000

    [add]モデルとテーブル、homesコントローラーとbooksコントローラーの作成。同時にビューページも自動作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/new' =>'books#new'
+  get 'books/index' =>'books#index'
+  get 'books/show' =>'books#show'
+  get 'books/edit' =>'books#edit'
+  get 'homes/top' =>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
