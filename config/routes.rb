40d175fc5553ea6c0e03d12abea00da585eef936
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-10 13:26:05 +0000

    [記述]投稿ページをビューで作って、モデルと連携し、コントローラーのnewアクションにインスタンス変数を定義した

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new' =>'books#new'
-  get 'books/index' =>'books#index'
-  get 'books/show' =>'books#show'
-  get 'books/edit' =>'books#edit'
-  get 'homes/top' =>'homes#top'
+  # get 'books' =>'books#new'
+  # post 'books'=>'books#create'
+  # get 'books/index' =>'books#index'
+  # get 'books/show' =>'books#show'
+  # get 'books/edit' =>'books#edit'
+    get 'homes/top' =>'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
