9f1c4bbba39c4d4518550c830eb24a25e84828b2
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-11 16:10:29 +0000

    [記述]showページ作成。一覧にshowのリンクを設定。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book=Book.new
-  end
   
   def create
     book=Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book)
   end
 
-　def index
-  　@books = Book.all
-　end
+  def index
+    @book=Book.new
+    @books = Book.all
+  end
 
   def show
-    @book = Book.find(params[:])
+    @book = Book.find(params[:id])
   end
 
   def edit
