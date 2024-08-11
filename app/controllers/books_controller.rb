7521a4cc2b130c6acae35fa8e670426a5904eea3
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-11 22:11:43 +0000

    [機能追加]editページ作成。一覧にeditのリンクを設定。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     book=Book.new(book_params)
     book.save
-    redirect_to book_path(book)
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -16,6 +16,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
